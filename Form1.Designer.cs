9ac2504666d17640c24d635d669f2ffa9ab13fdb
Petelkov <stanislavpetelkov@gmail.com>
2024-02-22 18:42:25 +0200

    SINGLY LINKED LIST.
    Date- 22-02-2024

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InsertNewNode = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(347, 33);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(75, 23);
+            this.printButton.TabIndex = 0;
+            this.printButton.Text = "Print";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(27, 87);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 196);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Принтирай едносвързан списък";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(279, 87);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(186, 196);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Случайни числа от 1 до 20";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Числа от 1 до 10";
+            // 
+            // InsertNewNode
+            // 
+            this.InsertNewNode.Location = new System.Drawing.Point(740, 33);
+            this.InsertNewNode.Name = "InsertNewNode";
+            this.InsertNewNode.Size = new System.Drawing.Size(140, 23);
+            this.InsertNewNode.TabIndex = 6;
+            this.InsertNewNode.Text = "Insert new node";
+            this.InsertNewNode.UseVisualStyleBackColor = true;
+            this.InsertNewNode.Click += new System.EventHandler(this.InsertNewNode_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(596, 110);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(237, 212);
+            this.listBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(705, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Число след дадена стойност";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(489, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Създай едносвързан списък";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(535, 53);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 10;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(525, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(692, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(805, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 360);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.InsertNewNode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.printButton);
+            this.Name = "Form1";
+            this.Text = "Едносвързан списък";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button InsertNewNode;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private LinkedList myList = new LinkedList();
+        public class Node
+        {
+            public int Data { get; set; }
+            public Node Next { get; set; }
+
+            public Node(int value)
+            {
+                Data = value;
+                Next = null;
+            }
+        }
+
+        public class LinkedList
+        {
+            private Node head;
+
+            public LinkedList()
+            {
+                head = null;
+            }
+
+            public void Insert(int value)
+            {
+                Node newNode = new Node(value);
+
+                if (head == null)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    Node current = head;
+                    while (current.Next != null)
+                    {
+                        current = current.Next;
+                    }
+                    current.Next = newNode;
+                }
+            }
+
+            public void InsertAfter(int existingValue, int newValue)
+            {
+                Node current = head;
+                while (current != null && current.Data != existingValue)
+                {
+                    current = current.Next;
+                }
+
+                if (current != null)
+                {
+                    Node newNode = new Node(newValue);
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                }
+                else
+                {
+                    MessageBox.Show("Няма такава стойност в списъка!");
+                }
+            }
+
+            public void Print(ListBox listBox)
+            {
+                Node current = head;
+                while (current != null)
+                {
+                    listBox.Items.Add(current.Data);
+                    current = current.Next;
+                }
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int newValue))
+            {
+                myList.Insert(newValue);
+                listBox3.Items.Clear();
+                myList.Print(listBox3);
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Въведете вярна стойност!");
+            }
+        }
+
+        private void InsertNewNode_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox2.Text, out int newValue) &&
+               int.TryParse(textBox3.Text, out int existingValue))
+            {
+                myList.InsertAfter(existingValue, newValue);
+                listBox3.Items.Clear();
+                myList.Print(listBox3);
+                textBox2.Clear();
+                textBox3.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Въведете вярна стойност!");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            // Izchistvame listbox 
+            listBox1.Items.Clear();
+
+            // Zapulvame lista s 10 sluchaini chisla ot 1 do 20 
+            HashSet<int> randNum = new HashSet<int>(); ;
+            Random rand = new Random();
+
+            while (randNum.Count < 10) // 10 sluchaini chisla
+            {
+                int randomNumber = rand.Next(1, 21);
+                while (randNum.Contains(randomNumber))
+                {
+                    randomNumber = rand.Next(1, 21);
+                }
+                randNum.Add(randomNumber);
+            }
+
+            // Sortirame chislata ot po-malko kum po-golqmo
+            var sortedNumbers = randNum.OrderBy(x => x);
+
+            // Otpechatvame chislata v listbox
+            foreach (int number in sortedNumbers)
+            {
+                listBox1.Items.Add(number);
+            }
+
+            {
+                // Izchistvame listbox
+                listBox2.Items.Clear();
+
+                // Suzdavame nov list
+                LinkedList myList = new LinkedList();
+
+                // Zapulvame lista s cifri
+                myList.Insert(1);
+                myList.Insert(2);
+                myList.Insert(3);
+                myList.Insert(4);
+                myList.Insert(5);
+                myList.Insert(6);
+                myList.Insert(7);
+                myList.Insert(8);
+                myList.Insert(9);
+                myList.Insert(10);
+
+                // Otpechatvame chislata
+                myList.Print(listBox2);
+            }
+        }
+    }
+}
